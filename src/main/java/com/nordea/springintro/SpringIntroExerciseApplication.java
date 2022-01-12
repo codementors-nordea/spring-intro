9a5b2c0d5c618a700aca9d12ab5c316bdac66a38
maciej.koziara <maciej.koziara@dac.digital>
2022-01-12 19:35:18 +0100

    Init commit

--- src/main/java/com/nordea/springintro/SpringIntroExerciseApplication.java
+++ src/main/java/com/nordea/springintro/SpringIntroExerciseApplication.java
@@ -1,4 +1,4 @@
-package pl.infoshare.springintro;
+package com.nordea.springintro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
