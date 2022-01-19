8c9aff483f8eb8737ede371b07f5c35e41807aad
maciej.koziara <maciej.koziara@dac.digital>
2022-01-19 16:15:38 +0100

    Add basic class

---
+++ src/main/java/pl/codementors/springintro/pets/PetController.java
@@ -0,0 +1,11 @@
+package pl.codementors.springintro.pets;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PetController {
+
+    // todo replace Object with concrete type
+    private final Map<Integer, Object> pets = new HashMap<>();
+
+}
